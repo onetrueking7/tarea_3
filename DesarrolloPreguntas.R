828c662c7af4c75ac628705eb5eb0f42ccb370ba
ariamp21 <ariamp21@gmail.com>
2019-09-28 10:17:24 -0400

    uj

---
+++ DesarrolloPreguntas.R
@@ -0,0 +1,21 @@
+listaDeNumeros<- list(2,5,6,2,1,5,6,10,11,20,15)
+listaDeNumeros[5]
+listaDeNumeros[0]
+unlist(listaDeNumeros[5])
+listaDeNumeros[5]
+unlist(listaDeNumeros[5])
+if(listaDeNumeros[5]+1>0){print('secumple')}
+if(unlist(listaDeNumeros[5])+1>0){print('secumple')}
+listaDeNumeros{5} <- 12
+length(listaDeNumeros)
+valorInicial<-5
+valorFinal<-20
+valorInicial:valorFinal
+valorInicial<-20
+valorFinal<-5
+valorInicial:valorFinal
+valorInicial:length(listaDeNumeros)
+valorInicial<-10
+valorFinal<-10
+valorInicial:valorFinal
+for(i in 1:100){print(paste('cuento',i,'misisipis'))}
