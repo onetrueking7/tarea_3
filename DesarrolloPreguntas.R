ae1244b5b123a9c945db4ebc005f5c08872e3473
onetrueking7 <55023508+onetrueking7@users.noreply.github.com>
2019-10-01 00:35:44 -0300

    Update DesarrolloPreguntas.R

--- DesarrolloPreguntas.R
+++ DesarrolloPreguntas.R
@@ -19,3 +19,20 @@ valorInicial<-10
 valorFinal<-10
 valorInicial:valorFinal
 for(i in 1:100){print(paste('cuento',i,'misisipis'))}
+for(i in listaDeNumeros){print(i)if(i%%2==0) {print(&quot;par&quot;)} else{print(&quot;impar&quot;)}
+  Total<-100
+VotosSi<-40  
+VotosNo<-30
+if(VotosSi>VotosNo){print('Gana Si')}else{print('Gana No')}
+Suma<-function(a,b){a+b}
+Suma(3,4)
+Multiplica<-function(a,b){a*b}
+Multiplica(2,6)
+Resta<-function(a,b){a-b}
+Resta(6,2)
+Divide<-function(a,b){a/b}
+Divide(10,5)
+area_rectangulo<-function(b1,h1,b2,h2){b1*h1-b2*h2}
+area_rectangulo(5,7,2,3)
+area_circulo<-function(r1,r2){abs((pi*r2^2))}
+area_circulo(6,4)
